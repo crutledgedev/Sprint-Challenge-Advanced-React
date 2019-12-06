4a1c3a787b57920e6fb56ebe29844f0aa19ab5df
Chance Rutledge <chancerutledge2@gmail.com>
2019-12-06 12:57:02 -0800

    MVP met

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+test('renders header', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/women's world cup/i);
+  // expect(linkElement).toBeInTheDocument();
 });
